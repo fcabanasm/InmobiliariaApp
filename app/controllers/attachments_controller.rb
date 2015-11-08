ffce9e3c739b578e4099d8e96a8c62aa553de487
fcabanasm <franco.cabanas@ceinf.cl>
2015-11-08 20:27:47 -0300

    "correccion errores attachment"

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -1,5 +1,6 @@
 class AttachmentsController < ApplicationController
   before_action :set_attachment, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:show, :index]
 
   # GET /attachments
   # GET /attachments.json
