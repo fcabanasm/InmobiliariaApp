b115e52928da7a9c10ed3f1eb3b3519bc8803a8c
fcabanasm <franco.cabanas@ceinf.cl>
2015-12-07 20:30:06 -0300

    Vistas

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,8 @@
 class CommentsController < ApplicationController
   before_action :set_comment, only: [:show, :edit, :update, :destroy]
-  before_action :set_apartment, only: [:create] 
+  before_action :set_apartment, only: [:create]
+  before_action :authenticate_user!, except: [:show, :index]
+
   # GET /comments
   # GET /comments.json
   def index
@@ -29,7 +31,7 @@ class CommentsController < ApplicationController
 
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to @comment.apartment, notice: 'Comment was successfully created.' }
+        format.html { redirect_to @comment.apartment, notice: 'El comentario fue publicado exitosamente.' }
         format.json { render :show, status: :created, location: @comment }
       else
         format.html { render :new }
@@ -43,7 +45,7 @@ class CommentsController < ApplicationController
   def update
     respond_to do |format|
       if @comment.update(comment_params)
-        format.html { redirect_to @comment.apartment, notice: 'Comment was successfully updated.' }
+        format.html { redirect_to @comment.apartment, notice: 'El comentario fue editado exitosamente.' }
         format.json { render :show, status: :ok, location: @comment }
       else
         format.html { render :edit }
@@ -57,7 +59,7 @@ class CommentsController < ApplicationController
   def destroy
     @comment.destroy
     respond_to do |format|
-      format.html { redirect_to @comment.apartment, notice: 'Comment was successfully destroyed.' }
+      format.html { redirect_to @comment.apartment, notice: 'El comentario fue eliminado exitosamente.' }
       format.json { head :no_content }
     end
   end
