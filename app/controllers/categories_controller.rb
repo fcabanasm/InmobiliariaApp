3b181f09bb3ff1487d3b165806cc9eaf40529672
eliascaceres <elias_2005_24@hotmail.com>
2015-12-08 12:39:13 -0300

    categoria y sus arriendos

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -28,7 +28,7 @@ class CategoriesController < ApplicationController
 
     respond_to do |format|
       if @category.save
-        format.html { redirect_to @category, notice: 'Categoria creada exitosamente.' }
+        format.html { redirect_to categories_path, notice: 'Categoria creada exitosamente.' }
         format.json { render :show, status: :created, location: @category }
       else
         format.html { render :new }
